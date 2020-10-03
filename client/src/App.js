26f2e90d495e166bc8694488aeb6b4e3dc00bb05
YessineAmor <yassine.amor@esprit.tn>
2020-10-04 00:40:18 +0100

    playing around with the api.tx

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 function App() {
+  let contractBytes;
+  fetch('guesserv3.wasm').then(async (res) => {
+    const buffer = await res.arrayBuffer();
+    console.log(buffer);
+    contractBytes = buffer;
+  });
+  const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+  ApiPromise.create({ provider: wsProvider }).then((api) => {
+    console.log('Connected to substrate node ' + api.genesisHash.toHex());
+    console.log(
+      'Putting contract code ' + api.tx.contracts.putCode(contractBytes)
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
